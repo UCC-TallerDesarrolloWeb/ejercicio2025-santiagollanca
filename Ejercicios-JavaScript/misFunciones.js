5e9f82e73e8856a89abcfeb822952d4dbe32b32c
santiago21 <2406584@ucc.edu.ar>
2025-09-15 09:41:24 -0300

    se realizaron los ejercicios de la clase

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,9 +1,8 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * Convierte las unidades ingresadas por el ususario
+ * @method convertirUnidades
+ * @param {string} unidad - Grados Raidanes
+ * @param {number} valor Valor numérico ingresado por el usuario (puede ser con coma)
  */
 
 function convertirUnidades(unidad, valor) {
@@ -46,4 +45,66 @@ function convertirUnidades(unidad, valor) {
     document.getElementById("pie").value = pie;
     document.getElementById("pulgada").value = pulgada;
     document.getElementById("yarda").value = yarda;
+}
+
+let convertirGR = (id, valor) => {
+    let cantGrados, cantRadianes;
+    if (id == "grados") {
+        cantGrados = valor;
+        cantRadianes = (valor * Math.PI) / 180;
+        document.getElementById("radianes").value = cantRadianes;
+    }else{
+        cantRadianes = valor;
+        cantGrados = (valor * 180) / Math.PI;
+        document.getElementById("grados").value = cantGrados;
+    }
+}
+
+let mostrarOcultar = (id) => {
+    if (id=="mostrarDiv"){
+        document.getElementsByName("unDiv")[0].style.display = "block";
+    }else{
+        document.getElementsByName("unDiv")[0].style.display = "none";
+    }
+}
+
+let sumar = () => {
+    let sum1 = document.getElementById("nums1").value;
+    let sum2 = document.getElementById("nums2").value;
+
+    if(isNaN(sum1) || isNaN(sum2)){
+        alert("Los valores ingresados no son correctos");
+    }else{
+    document.getElementById("totalS").value = Number(sum1) + Number(sum2);
+    }
+}
+
+let restar = () => {
+    let res1 = document.getElementById("numr1").value;
+    let res2 = document.getElementById("numr2").value;
+    if(isNaN(res1) || isNaN(res2)){
+        alert("Los valores ingresados no son correctos");
+    }else{
+    document.getElementById("totalR").value = Number(res1) - Number(res2);
+    }
+}
+
+let multiplicar = () => {
+    let mul1 = document.getElementById("numm1").value;
+    let mul2 = document.getElementById("numm2").value;
+    if(isNaN(mul1) || isNaN(mul2)){
+        alert("Los valores ingresados no son correctos");
+    }else{
+    document.getElementById("totalM").value = Number(mul1) * Number(mul2);
+    }
+}
+
+let dividir = () => {
+    let div1 = document.getElementById("numd1").value;
+    let div2 = document.getElementById("numd2").value;
+    if(isNaN(div1) || isNaN(div2)){
+        alert("Los valores ingresados no son correctos");
+    }else{
+    document.getElementById("totalD").value = Number(div1) / Number(div2);
+    }
 }
