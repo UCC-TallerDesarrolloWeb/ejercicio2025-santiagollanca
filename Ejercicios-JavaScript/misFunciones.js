6c1ae3b053e194f577ae584a53bf15f77f48d30a
santiago21 <2406584@ucc.edu.ar>
2025-11-07 11:55:35 -0300

    se corrigieron los ejercicios de java

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,114 +1,85 @@
 /**
- * Convierte las unidades ingresadas por el ususario
+ * Convierte las unidades ingresadas por el usuario
  * @method convertirUnidades
- * @param {string} unidad - Grados Raidanes
- * @param {number} valor Valor numérico ingresado por el usuario (puede ser con coma)
+ * @param {string} unidad - Unidad ingresada: metro, pie, pulgada, yarda
+ * @param {number} valor - Valor numérico ingresado por el usuario (puede ser con coma)
  */
+let convertirUnidades = (unidad, valor) => {
+  let metro, pie, pulgada, yarda;
 
-function convertirUnidades(unidad, valor) {
-    let metro, pie, pulgada, yarda;
+  if (valor.includes(",")) {
+    valor = valor.replace(",", ".");
+  }
 
-    if(String(valor).includes(",")){
-        valor = valor.replace(",",".");
-    }
-    
-    console.log(valor);
-    console.log(isNaN(valor));
-
-    if (isNaN(valor)) {
-        alert("El valor ingresado no es correcto");
-        metro = "";
-        pie = "";
-        pulgada = "";
-        yarda = "";
-    }
-
+  if (isNaN(valor)) {
+    alert("El valor ingresado no es correcto");
+    metro = "";
+    pie = "";
+    pulgada = "";
+    yarda = "";
+  } else {
     if (unidad == "unid_metro") {
-        metro = valor;
-        pie = valor * 3.28084;
-        pulgada = valor * 39.3701;
-        yarda = valor * 1.09361;
-    }else if (unidad == "unid_pie") {  
-        pie = valor;
-        metro = valor / 3.28084;
-        pulgada = valor * 12;
-        yarda = valor / 3;
-    }else if (unidad == "unid_pulgada") {
-        pulgada = valor;
-        metro = valor / 39.3701;
-        pie = valor / 12;
-        yarda = valor / 36;
-    }else if (unidad == "unid_yarda") {
-        yarda = valor;
-        metro = valor / 1.09361;
-        pie = valor * 3;
-        pulgada = valor * 36;
+      metro = valor;
+      pie = 3.28 * metro;
+      pulgada = 39.37 * metro;
+      yarda = 1.0936 * metro;
+    } else if (unidad == "unid_pie") {
+      pie = valor;
+      metro = 0.3048 * pie;
+      pulgada = 12 * pie;
+      yarda = 0.3333 * pie;
+    } else if (unidad == "unid_pulgada") {
+      pulgada = valor;
+      //tarea
+    } else {
+      yarda = valor;
+      //tarea
     }
+  }
 
-    document.getElementById("metro").value = metro.toFixed(2);
-    document.getElementById("pie").value = Math.round(pie*100)/100;
-    document.getElementById("pulgada").value = pulgada.toFixed(2);
-    document.getElementById("yarda").value = Math.round(yarda*100)/100;
-}
+  document.getElementById("metro").value = Number(metro).toFixed(2);
+  document.getElementById("pie").value = Math.round(pie * 100) / 100;
+  document.getElementById("pulgada").value = Number(pulgada).toFixed(2);
+  document.getElementById("yarda").value = Math.round(yarda * 100) / 100;
+};
 
+/**
+ * Convierte grados a radianes y viceversa
+ * @method convertirGR
+ * @param {string} id - Grados o Radianes
+ * @param {number} valor - Valor numérico ingresado por el usuario (puede ser con coma)
+ */
 let convertirGR = (id, valor) => {
-    let cantGrados, cantRadianes;
-    if (id == "grados") {
-        cantGrados = valor;
-        cantRadianes = (valor * Math.PI) / 180;
-        document.getElementById("radianes").value = cantRadianes;
-    }else{
-        cantRadianes = valor;
-        cantGrados = (valor * 180) / Math.PI;
-        document.getElementById("grados").value = cantGrados;
-    }
-}
+  let cantGrados, cantRadianes;
+  if (id == "grados") {
+    cantGrados = valor;
+    cantRadianes = (cantGrados * Math.PI) / 180;
+    document.getElementById("radianes").value = cantRadianes;
+  } else {
+    cantRadianes = valor;
+    cantGrados = (cantRadianes * 180) / Math.PI;
+    document.getElementById("grados").value = cantGrados;
+  }
+};
 
-let mostrarOcultar = (id) => {
-    if (id=="mostrarDiv"){
-        document.getElementsByName("unDiv")[0].style.display = "block";
-    }else{
-        document.getElementsByName("unDiv")[0].style.display = "none";
-    }
-}
+let mostrarOcultarDiv = (id) => {
+  // if (id == "mostrarDiv") {
+  //   document.getElementsByName("unDiv")[0].style.display = "block";
+  // } else {
+  //   document.getElementsByName("unDiv")[0].style.display = "none";
+  // }
+
+  const mostrar = id == "mostrarDiv" ? "block" : "none";
+  document.getElementsByName("unDiv")[0].style.display = mostrar;
+};
 
 let sumar = () => {
-    let sum1 = document.getElementById("nums1").value;
-    let sum2 = document.getElementById("nums2").value;
+  let sum1 = document.getElementById("nums1").value;
+  let sum2 = document.getElementById("nums2").value;
 
-    if(isNaN(sum1) || isNaN(sum2)){
-        alert("Los valores ingresados no son correctos");
-    }else{
+  if (isNaN(sum1) || isNaN(sum2)) {
+    alert("Una de las variables ingresadas no es numérica");
+  } else {
     document.getElementById("totalS").value = Number(sum1) + Number(sum2);
-    }
-}
-
-let restar = () => {
-    let res1 = document.getElementById("numr1").value;
-    let res2 = document.getElementById("numr2").value;
-    if(isNaN(res1) || isNaN(res2)){
-        alert("Los valores ingresados no son correctos");
-    }else{
-    document.getElementById("totalR").value = Number(res1) - Number(res2);
-    }
-}
-
-let multiplicar = () => {
-    let mul1 = document.getElementById("numm1").value;
-    let mul2 = document.getElementById("numm2").value;
-    if(isNaN(mul1) || isNaN(mul2)){
-        alert("Los valores ingresados no son correctos");
-    }else{
-    document.getElementById("totalM").value = Number(mul1) * Number(mul2);
-    }
-}
-
-let dividir = () => {
-    let div1 = document.getElementById("numd1").value;
-    let div2 = document.getElementById("numd2").value;
-    if(isNaN(div1) || isNaN(div2)){
-        alert("Los valores ingresados no son correctos");
-    }else{
-    document.getElementById("totalD").value = Number(div1) / Number(div2);
-    }
-}
+  }
+};
--- Ejercicios-JavaScript/tienda.js
+++ Ejercicios-JavaScript/tienda.js
@@ -15,7 +15,7 @@ const productos = [
     categoria: "Dobok",
     marca: "Daedo",
     talle: ["1", "2", "3", "4", "5", "6", "7", "8"],
-    precio: 115000,
+    precio: 115000.345,
     web: "https://www.daedo.com/products/taitf-10813",
     imagen: "dobok.webp",
   },
@@ -25,7 +25,7 @@ const productos = [
     categoria: "Entrenamiento",
     marca: "Gran Marc",
     talle: ["s/talle"],
-    precio: 51700,
+    precio: 51700.23,
     web: "https://www.granmarctiendaonline.com.ar/productos/escudo-de-potencia-grande/",
     imagen: "escudo-potencia.webp",
   },
@@ -46,7 +46,7 @@ const productos = [
     categoria: "Protectores",
     marca: "Daedo",
     talle: ["s/talle"],
-    precio: 35000,
+    precio: 35000.5,
     web: "https://www.daedo.com/products/pritf-2020",
     imagen: "protectores-manos.webp",
   },
@@ -58,22 +58,263 @@ const productos = [
     talle: ["XXS", "XS", "S", "M", "L", "XL"],
     precio: 35000,
     web: "https://www.daedo.com/collections/collection-itf-gloves/products/pritf-2022",
-    imagen: "protectores-manos.webp",
+    imagen: "protectores-pie.webp",
   },
 ];
 
-let mostrarDetalle = () => {
+/**
+ * Descripción de que hace la función
+ * @method mostrarDetalle
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let mostrarDetalle = (id) => {
   document.getElementById("detalle").style.display = "block";
-}
+  document.getElementById("titulo-prod").innerText = productos[id].nombre;
+  document.getElementById("descr-prod").innerText = productos[id].description;
+  document.getElementById("precio-prod").innerText = productos[id].precio;
+};
 
+/**
+ * Descripción de que hace la función
+ * @method mostrarDetalle
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
 let cerrarModal = () => {
   document.getElementById("detalle").style.display = "none";
-}
+};
 
-let mostrarCatalogo = () => {
+/**
+ * Descripción de que hace la función
+ * @method mostrarDetalle
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let mostrarCatalogo = (prod = productos) => {
   let contenido = "";
-  productos.forEach((prod) => {
-  })
-}
 
-document.getElementById("catalogo").innerHTML = contenido;
+  prod.forEach((prod, id) => {
+    contenido += `<div>
+        <img src="images/${prod.imagen}" alt="${prod.nombre}" />
+        <h3>${prod.nombre}</h3>
+        <p>${formatPrice(prod.precio)}</p>
+        <button type="button" onclick="mostrarDetalle(${id})">Ver Detalle</button>
+        <button type="button" onclick="agregarAlCarrito(${id})">Agregar al Carrito</button>
+      </div>`;
+  });
+
+  document.getElementById("catalogo").innerHTML = contenido;
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let agregarAlCarrito = (id) => {
+  let listadoProductos;
+  const listaInicial = JSON.parse(localStorage.getItem("carrito"));
+
+  if (listaInicial == null) {
+    listadoProductos = [];
+  } else {
+    listadoProductos = listaInicial;
+  }
+
+  listadoProductos.push(id);
+  localStorage.setItem("carrito", JSON.stringify(listadoProductos));
+  contarProductos();
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let mostrarCarrito = () => {
+  let contenido = "";
+  const carrito = JSON.parse(localStorage.getItem("carrito"));
+  let total = 0;
+
+  if (carrito != null) {
+    const listProd = [];
+    const listCant = [];
+
+    carrito.forEach((num) => {
+      if (!listProd.includes(num)) {
+        listProd.push(num);
+        listCant.push(1);
+      } else {
+        const inx = listProd.indexOf(num);
+        listCant[inx] += 1;
+      }
+    });
+
+    listProd.forEach((num, id) => {
+      contenido += `<div>
+        <h3>${productos[num].nombre}</h3>
+        <p>${formatPrice(productos[num].precio)}</p>
+        <p>Cantidad: ${listCant[id]}</p>
+        <button type="button" onclick="eliminarProducto(${id})">Eliminar Producto</button>
+      </div>`;
+      total += productos[num].precio * listCant[id];
+    });
+
+    contenido += `<p>Total= ${formatPrice(total)}</p>`;
+    contenido += `<button type="button" onClick="vaciarCarrito()">Vaciar Carrito</button>`;
+    document.getElementById("carrito").innerHTML = contenido;
+  }
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let vaciarCarrito = () => {
+  localStorage.removeItem("carrito");
+  window.location.reload();
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let eliminarProducto = (id) => {
+  const carrito = JSON.parse(localStorage.getItem("carrito"));
+
+  carrito.splice(id, 1);
+
+  if (carrito.length > 0) {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  } else {
+    localStorage.removeItem("carrito");
+  }
+
+  window.location.reload();
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let filtrarProducto = () => {
+  //Mejorar y Optimizar esta función (4 funciones)
+  let searchWord = document.getElementById("search").value;
+  let min = document.getElementById("price-min").value;
+  let max = document.getElementById("price-max").value;
+  let prot = document.getElementById("protectores").checked;
+  let entr = document.getElementById("entrenamiento").checked;
+  let dob = document.getElementById("dobok").checked;
+  let marca = document.getElementById("marca").value;
+
+  let newLista = productos;
+
+  if (searchWord) {
+    newLista = newLista.filter(
+      (prod) =>
+        prod.nombre.toLowerCase().includes(searchWord.toLowerCase()) ||
+        prod.description.toLowerCase().includes(searchWord.toLowerCase())
+    );
+  }
+  if (min) {
+    newLista = newLista.filter((prod) => prod.precio >= min);
+  }
+  if (max) {
+    newLista = newLista.filter((prod) => prod.precio <= max);
+  }
+
+  let category = [];
+  prot ? category.push("Protectores") : "";
+  entr ? category.push("Entrenamiento") : "";
+  dob ? category.push("Dobok") : "";
+
+  if (category.length > 0) {
+    newLista = newLista.filter((prod) => category.includes(prod.categoria));
+  }
+
+  if (marca != "Todas") {
+    newLista = newLista.filter((prod) => prod.marca == marca);
+  }
+
+  mostrarCatalogo(newLista);
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let formatPrice = (price) => {
+  const numberFormat = new Intl.NumberFormat("es-AR", {
+    currency: "ARS",
+    style: "currency",
+  });
+  return numberFormat.format(price);
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let contarProductos = () => {
+  const getCart = localStorage.getItem("carrito");
+
+  if (getCart != null) {
+    document.getElementById("cant-prod").innerText = JSON.parse(getCart).length;
+  }
+};
+
+/**
+ * Descripción de que hace la función
+ * @method agregarAlCarrito
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @return Valor que retorna
+ */
+let orderCatalog = (order) => {
+  //Optimizar la función
+  let newProducts;
+
+  switch (order) {
+    case "menor":
+      newProducts = productos.sort((a, b) => a.precio - b.precio);
+      break;
+    case "mayor":
+      newProducts = productos.sort((a, b) => b.precio - a.precio);
+      break;
+    case "a-z":
+      newProducts = productos.sort((a, b) => {
+        if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      break;
+    case "z-a":
+      newProducts = productos.sort((a, b) => {
+        if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      break;
+    default:
+      newProducts = productos.sort((a, b) => a.precio - b.precio);
+      break;
+  }
+
+  mostrarCatalogo(newProducts);
+};
